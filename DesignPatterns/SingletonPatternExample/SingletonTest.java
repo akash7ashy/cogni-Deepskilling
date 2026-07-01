1a6ae3c15303600ff88fcc39c575fddc48db7016
Akash.R <131885338+akash7ashy@users.noreply.github.com>
2026-07-01 21:12:41 +0530

    Update SingletonTest.java

--- DesignPatterns/SingletonPatternExample/SingletonTest.java
+++ DesignPatterns/SingletonPatternExample/SingletonTest.java
@@ -2,15 +2,15 @@ public class SingletonTest {
 
     public static void main(String[] args) {
 
-        // Get the first instance
+       
         Logger logger1 = Logger.getInstance();
         logger1.log("First log message.");
 
-        // Get the second instance
+       
         Logger logger2 = Logger.getInstance();
         logger2.log("Second log message.");
 
-        // Check whether both references point to the same object
+        
         if (logger1 == logger2) {
             System.out.println("Both logger1 and logger2 refer to the same instance.");
         } else {
