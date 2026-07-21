9776a106f0284eba7ccb40a093da9744f120da12
akash7ashy <13r.akash@gmail.com>
2026-07-21 22:12:20 +0530

    files added

---
+++ Week1/BuilderPatternExample/DataStructureandAlgorithm/Ex2-EcommercePlatformfunction/EcommerceSearch.java
@@ -0,0 +1,53 @@
+public class EcommerceSearch {
+
+    public static Product linearSearch(Product[] products, int targetId) {
+        for (Product product : products) {
+            if (product.productId == targetId) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static Product binarySearch(Product[] products, int targetId) {
+        int low = 0;
+        int high = products.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (products[mid].productId == targetId) {
+                return products[mid];
+            }
+
+            if (products[mid].productId < targetId) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+            new Product(101, "Laptop", "Electronics"),
+            new Product(102, "Phone", "Electronics"),
+            new Product(103, "Shoes", "Fashion"),
+            new Product(104, "Watch", "Accessories"),
+            new Product(105, "Bag", "Fashion")
+        };
+
+        int searchId = 104;
+
+        Product linearResult = linearSearch(products, searchId);
+        System.out.println("Linear Search Result:");
+        System.out.println(linearResult);
+
+        Product binaryResult = binarySearch(products, searchId);
+        System.out.println("\nBinary Search Result:");
+        System.out.println(binaryResult);
+    }
+}
